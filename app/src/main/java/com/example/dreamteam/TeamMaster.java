94e10a492ea3f39a1a9f50fd034e1c2b74be17c5
ACrst <aiona17@hotmail.com>
2021-05-17 15:56:51 -0400

    Team Master class update.

---
+++ app/src/main/java/com/example/dreamteam/TeamMaster.java
@@ -0,0 +1,22 @@
+package com.example.dreamteam;
+
+public class TeamMaster {
+    public int teamid;
+    public int TeamName;
+
+    public int getTeamid() {
+        return teamid;
+    }
+
+    public void setTeamid(int teamid) {
+        this.teamid = teamid;
+    }
+
+    public int getTeamName() {
+        return TeamName;
+    }
+
+    public void setTeamName(int teamName) {
+        TeamName = teamName;
+    }
+}
