cd4851735ae583b636d2a33a5baf024946b20c3d
Melanie Crasta <mcrasta1@umbc.edu>
2021-05-18 21:34:46 -0400

    commit b4 stash

--- app/src/main/java/com/example/dreamteam/Page1.java
+++ app/src/main/java/com/example/dreamteam/Page1.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -26,13 +27,11 @@ public class Page1 extends AppCompatActivity implements View.OnClickListener {
         joinTeamButton.setOnClickListener(this);
         createTeamButton = (Button) findViewById(R.id.createTeamButton);
         createTeamButton.setOnClickListener(this);
-
-
-
     }
 
     @Override
     public void onClick(View view) {
+        Log.e("FIREBASE", "Hwlllllll");
         switch (view.getId()){
             //If the User clicks on joinTeam, they are taken to page 2.
             case R.id.joinTeamButton:
