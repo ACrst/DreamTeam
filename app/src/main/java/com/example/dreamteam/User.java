15d9f3cbff101d79d38343a188c49d1ef87320b5
ACrst <aiona17@hotmail.com>
2021-05-20 16:22:33 -0400

    User class update.

--- app/src/main/java/com/example/dreamteam/User.java
+++ app/src/main/java/com/example/dreamteam/User.java
@@ -1,14 +1,27 @@
 package com.example.dreamteam;
+
+import java.io.Serializable;
+
 //class to record User responses
-public class User {
+public class User implements Serializable {
     private String userName;
+    private String roompin;
     private int subTeamID; //The subTeam that they belong to, that is the teamId of the team of the team they belong to. Can only be 1,2,3,4
     private int question1Answer; //This will hold the id of the option they chose for each q.(It is the optionID of the option they choose)
     private int question2Answer;
     private int question3Answer;
 
-    public User(String uname){
+    public User(String uname,String rpin){
         this.userName = uname;
+        this.roompin=rpin;
+    }
+
+    public String getRoompin() {
+        return roompin;
+    }
+
+    public void setRoompin(String roompin) {
+        this.roompin = roompin;
     }
 
     //Getters
